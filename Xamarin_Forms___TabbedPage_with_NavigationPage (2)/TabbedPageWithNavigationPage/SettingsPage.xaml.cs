24ef74aca59838bcdeef09bfd75ef7b25cdfbb74
Daniel Karll <danieljkarll@gmail.com>
2020-04-08 15:42:46 -0500

    Lab 9 completed

---
+++ Xamarin_Forms___TabbedPage_with_NavigationPage (2)/TabbedPageWithNavigationPage/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+		}
+		private void CheckUserAge(object sender, EventArgs e)
+		{
+			string sMessage = "";
+			if (Convert.ToInt32(userAge.Text) < 16)
+			{
+				sMessage = "You can't drive yet";
+			}
+			else
+			{
+				sMessage = "You have been able to drive legally in wisconsin for a total of " + (Convert.ToInt32(userAge.Text) - 16) + " years!";
+			}
+			totalYearsMsg.Text = sMessage;
+			totalYearsMsg.IsVisible = true;
+		}
+
+	}
+}
+
---
+++ Xamarin_Forms___TabbedPage_with_NavigationPage (2)/TabbedPageWithNavigationPage/SettingsPageCS.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace TabbedPageWithNavigationPage
+{
+	public class SettingsPageCS : ContentPage
+	{
+		public SettingsPageCS ()
+		{
+			
+			Title = "Check Age";
+			Content = new StackLayout {
+				Children = {
+					new Label {
+						Text = "Settings go here",
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.CenterAndExpand
+					}
+				}
+			};
+		}
+	}
+}
